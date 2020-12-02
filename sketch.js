e224bb01ceea37a50d1e2b15d1ca1cb3d6aa48fe
PrishaMadane <70759166+PrishaMadane@users.noreply.github.com>
2020-12-02 17:07:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,40 +13,29 @@ function preload()
 
 function setup() {
 	createCanvas(1000, 700);
-
+  rectMode(CENTER);
 
 	engine = Engine.create();
 	world = engine.world;
-	Engine.run(engine);
-	
-	paperObj= new Paper(200,600,15);
-  groundObj = new Ground(500,650,1000,10);
-  
-  redBox= createSprite(700, 630,200,20);
-  redBox.shapeColor="white";
-
-  redBox2=createSprite(790, 600,20,70);
-  redBox2.shapeColor="white";
-
-  redBox3=createSprite(610, 600,20,70);
-  redBox3.shapeColor="white";
-
-
+  dustbinObj1= new dustbin(1200,650);
+  paperObj= new paper(200,600,15);
+  groundObj = new ground(width/2,650,1000,10);
   
   
+  
+
+  
+  Engine.run(engine);
 }
-
 
 function draw() {
   Engine.update(engine);
   background(0);
-  groundObj.display();
+  groundObj.display(); 
   paperObj.display();
-  redBox.display();
-  redBox2.display();
-  redBox3.display();
+  dustbinObj1.display();
  
-  rectMode(CENTER);
+ 
 
   drawSprites();
 
@@ -54,7 +43,8 @@ function draw() {
 
 function keyPressed(){
   if(keyCode=== UP_ARROW){
-    Matter.Body.applyForce(paperObj.body,paperObj.body.position,{x:85,y:85});
+    Matter.Body.applyForce(paperObj.body,paperObj.body.position,{x:20,y:-20});
   }
 }
+
 
